701ca2e2fa623887a007f15639a4f4c6b3ee26a4
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-05-11 08:20:15 +0000

    데이터

--- src/Loign.jsx
+++ src/Loign.jsx
@@ -1,8 +1,18 @@
 
 
 const Login = () => {
-    const handleAuthClick =() => {
-        const API_KEY = ""
+    const mall_id = "whitecandy777"
+    const client_id = "p53jdXVUGVvvP2dVRZS9RD"
+    const scope = "mall.read_category,mall.write_category"
+    const encode_redirect_uri = "https://ssttest.netlify.app/auth/callback"
+    const encode_csrf_token = "asddada"
+    const url =  `https://{mall_id}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id={client_id}&state={encode_csrf_token}&redirect_uri={encode_redirect_uri}&scope={scope}`
+    const handleAuthClick = async() => {
+      const data = await fetch(url, {
+        method:"POST"
+      })
+      const json = await data.json();
+      console.log(json)
     }
     return(
         <div>
