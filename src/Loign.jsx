6fa5a89d52ed4a2b68ff975a6c5c08396927d915
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-05-11 08:23:06 +0000

    오타수정

--- src/Loign.jsx
+++ src/Loign.jsx
@@ -6,7 +6,7 @@ const Login = () => {
     const scope = "mall.read_category,mall.write_category"
     const encode_redirect_uri = "https://ssttest.netlify.app/auth/callback"
     const encode_csrf_token = "asddada"
-    const url =  `https://{mall_id}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id={client_id}&state={encode_csrf_token}&redirect_uri={encode_redirect_uri}&scope={scope}`
+    const url =  `https://${mall_id}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=${client_id}&state=${encode_csrf_token}&redirect_uri=${encode_redirect_uri}&scope=${scope}`
     const handleAuthClick = async() => {
       const data = await fetch(url, {
         method:"POST"
