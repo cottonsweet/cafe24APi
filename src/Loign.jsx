66d158e6c8dccaf15f147441355b57f650ae986e
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-05-11 08:29:41 +0000

    tset' '

--- src/Loign.jsx
+++ src/Loign.jsx
@@ -1,11 +1,11 @@
 
 
 const Login = () => {
-    const mall_id = "whitecandy777"
-    const client_id = "p53jdXVUGVvvP2dVRZS9RD"
-    const scope = "mall.read_category,mall.write_category"
+    const mall_id = process.env.MALL_ID || "whitecandy777"
+    const client_id = process.env.CLIENT_ID || "p53jdXVUGVvvP2dVRZS9RD"
+    const scope = process.env.SCOPE || "mall.read_category,mall.write_category"
     const encode_redirect_uri = "https://ssttest.netlify.app/auth/callback"
-    const encode_csrf_token = "asddada"
+    const encode_csrf_token = process.env.CSRF_TOKEN || "asddada"
     const url =  `https://${mall_id}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=${client_id}&state=${encode_csrf_token}&redirect_uri=${encode_redirect_uri}&scope=${scope}`
     const handleAuthClick = async() => {
       const data = await fetch(url)
